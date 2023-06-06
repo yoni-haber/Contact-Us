c8d5a86f2f8e3e4dbcde971489fd01c76d00da5f
Yoni Haber <YHaber@regatta.com>
2023-06-06 15:28:44 +0100

    HTML form template re-design and connection to database established

---
+++ form.php
@@ -0,0 +1,37 @@
+<?php
+if(isset($_POST['submit']))
+    {
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+        $email = $_POST['email'];
+        $subject = $_POST['subject'];
+        $message = $_POST['msg'];
+ 
+        //database details. You have created these details in the third step. Use your own.
+        $host = "localhost";
+        $username = "formdb_user";
+        $password = "cucumberband123";
+        $dbname = "form_entriesdb";
+ 
+        //create connection
+        $con = mysqli_connect($host, $username, $password, $dbname);
+        //check connection if it is working or not
+        if (!$con)
+        {
+            die("Connection failed!" . mysqli_connect_error());
+        }
+        //This below line is a code to Send form entries to database
+        $sql = "INSERT INTO contactform_entries (firstname_fld, lastname_fld, email_fld, sub_fld, msg_fld) VALUES ('$firstname', '$lastname', '$email', '$subject', '$message')";
+      //fire query to save entries and check it with if statement
+        $rs = mysqli_query($con, $sql);
+        if($rs)
+        {
+            echo "Message has been sent successfully!";
+        }
+      	else{
+         	echo "Error, Message didn't send! Something's Wrong."; 
+        }
+      //connection closed.
+        mysqli_close($con);
+    }
+?>
--- index.php
+++ index.php
@@ -1,37 +1,55 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en-US">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Form</title>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>HTML Contact Form</title>
+<link rel="stylesheet" href="style.css" type="text/css">
 </head>
 <body>
-    <form action="" method=""POST>
+<div class="form">
+    <form method="POST" action="form.php">
         <h1>Contact Us!</h1>
-        <label for="first-name">First Name:</label>
-        <input type="text" name="first-name" id="first-name" placeholder="Type here..." required pattern="[a-zA-Z]+">
-        <br>
-        <br>
-        <label for="last-name">Last Name:</label>
-        <input type="text" name="last-name" id="last-name" placeholder="Type here..." required pattern="[a-zA-Z]+">
-        <br>
-        <br>
-        <label for="email">Email Address:</label>
-        <input type="email" name="email" id="email" placeholder="Type here..." required>
-        <br>
-        <br>
-        <label for="subject">Subject:</label>
-        <input type="text" name="subject" id="subject" placeholder="Type here..." required>
-        <br>
-        <br>
-        <label for="message">Message:</label>
-        <br>
-        <textarea rows="5" cols="30" id="message" name="message" required placeholder="Type here..." minlength="2"></textarea>
-        <br>
-        <br>
-        <input type="reset">
-        <input type="submit" value="Send">
+        <table>
+            <tr>
+                <td>
+                    <label for="firstname">First Name:</label><br>
+                    <input type="text" name="firstname" placeholder="John" id="" required>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="lastname">Last Name:</label><br>
+                    <input type="text" name="lastname" placeholder="Doe" id="" required>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="email">Your Email:</label><br>
+                    <input type="email" name="email" placeholder="example@gmail.com" id="" required>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="subject">Subject:</label><br>
+                    <input type="text" name="subject" placeholder="Subject" id="" required>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="msg">Your Message:</label><br>
+                    <textarea name="msg" placeholder="Type your message..." id="" cols="30" rows="10" required></textarea>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="reset">
+                    <input type="submit" name="submit" value="Send Message">
+                </td>
+            </tr>
+        </table>
     </form>
+</div>
 </body>
 </html>
